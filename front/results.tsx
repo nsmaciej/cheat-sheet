3710fa14f558e3cb30f3fce4daf197905b7a6367
Maciej Goszczycki <mgoszcz2@gmail.com>
2018-09-16 06:02:19 -0400

    Make everything much better v2

--- front/results.tsx
+++ front/results.tsx
@@ -1,284 +1,75 @@
 import * as React from 'react'
 import styled from 'styled-components';
 
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+`
 
-const Filename = styled.h1`
-    font-size: 24px;
-    color: blue;
-`;
+const RepoName = styled.h2`
+    color: red;
+    margin-bottom: 20px;
+`
 
-// const PrintButton = styled.button`
-//     padding: 2em;
-//     text-align: center;
-//     text-decoration: none;
-// `;
+const FileContainer = styled.div`
+    margin-bottom: 40px;
+`
+const FileName = styled.div`
+    margin-top: 10px;
+    color: white;
+    background: red;
+    font-weight: bolder;
+    padding: 6px;
+    font-family: 'Roboto Mono', monospace;
+`
 
-const FunctionKeyword = styled.span`
-    font-size: 20px;
-    color: #5f6063;
-    font-weight: bold;
-`;
+const FunctionName = styled.div`
+    padding: 7px 0 0;
+`
 
-const Bold = styled.span`
-    font-weight: bold;
-`;
-
-const FunctionName = styled.span`
-    font-size: 20px;
-    color: #76777a;
-`;
-
-const StructName = styled.span`
-    font-size: 20px;
-    color: #76777a;
-`;
-
-
-const AllWrapper = styled.div`
-    background: #ccccff;
-    height: 100%;
-    width: 100%;
-`;
-
-
-const FileWrapper = styled.div`
-    padding: 3em;
-`;
-
-const FunctionsWrapper = styled.div`
-    background: #e8edf4;
-    padding: 1em;
-    border-radius: 20px;
-`;
-
-const TypesWrapper = styled.div`
-    background: #e8edf4;
-    padding: 1em;
-    border-radius: 20px;
-`;
-
-const Parameters = styled.span`
-    font-weight: light;
-    font-size: 18px;
-    font-style: italic;
-`;
-
-const ReturnValue = styled.span`
-    color:red;
-    font-weight: heavy;
-    font-size: 18px;
-    font-style: italic;
-`;
-
-const Heading = styled.span`
-    font-weight: heavy;
-    font-size: 18px;
-`;
-
-const StructKeyword = styled.span`
-    font-size: 20px;
-    color: #5f6063;
-    font-weight: bold;
-`;
-
-
-type Props = {
-    repo: Object,
-}
-
-type State = {
-    responseReceived: bool,
-}
-
-export class Results extends React.Component<Props, any> {
-    constructor(props){
+export class Results extends React.Component<{ repo: string }, { results: any }> {
+    constructor(props) {
         super(props);
-        this.state = {
-            detectedLanguages: [],
-            response: "",
-        }
-    }
-    
-    render(){
-        let detectedLanguages = ["Go"];
-        let repository = this.props.repo;
-        if (this.state.response.length === 0){
-            let xmlHttp = new XMLHttpRequest();
-            xmlHttp.open( "GET", "localhost:8080/", false ); 
-            xmlHttp.send( null );
-            let response = JSON.parse(xmlHttp.responseText);
-            this.setState({response: response});
-            console.log("HEY");
-            console.log(this.state.response);
-        }
-        else{
-            let files = this.state.response.map((file, i) => File(file));
-            return <AllWrapper>
-                {this.renderDropdown(detectedLanguages)}
-                {this.renderPrintButton()}
-                {files}
-            </AllWrapper>;
-
-        }
-        // let fake_results = {
-        //     "files" : [
-        //         {
-        //             "name": "src.go",
-        //             "functions": [
-        //                 {
-        //                     "name": "FindTheBeaver",
-        //                     "parameters": ["int", "string"],
-        //                     "returns": ["bool"],
-        //                 },
-        //                 {
-        //                     "name": "HideTheBeaver",
-        //                     "parameters": ["int", "string"],
-        //                     "returns": ["bool"],
-        //                 }
-        //             ],
-        //             "types": [
-        //                 {
-        //                     "name": "Eoin",
-        //                     "fields": [
-        //                         {
-        //                             "name": "height",
-        //                             "type": "float",
-        //                         },
-        //                         {
-        //                             "name": "weight",
-        //                             "type": "float",
-        //                         }
-        //                     ]
-        //                 },
-        //                 {
-        //                     "name": "Maciej",
-        //                     "fields": [
-        //                         {
-        //                             "name": "height",
-        //                             "type": "float",
-        //                         },
-        //                         {
-        //                             "name": "weight",
-        //                             "type": "float",
-        //                         }
-        //                     ]
-        //                 }
-        //             ]
-        //         },
-        //         {
-        //             "name": "lol.go",
-        //             "functions": [
-        //                 {
-        //                     "name": "Eat",
-        //                     "parameters": ["int", "string"],
-        //                     "returns": ["bool"],
-        //                 },
-        //                 {
-        //                     "name": "Drink",
-        //                     "parameters": ["int", "string"],
-        //                     "returns": ["bool"],
-        //                 }
-        //             ],
-        //             "types": [
-        //                 {
-        //                     "name": "Eoin",
-        //                     "fields": [
-        //                         {
-        //                             "name": "height",
-        //                             "type": "float",
-        //                         },
-        //                         {
-        //                             "name": "weight",
-        //                             "type": "float",
-        //                         }
-        //                     ]
-        //                 },
-        //                 {
-        //                     "name": "Maciej",
-        //                     "fields": [
-        //                         {
-        //                             "name": "height",
-        //                             "type": "float",
-        //                         },
-        //                         {
-        //                             "name": "weight",
-        //                             "type": "float",
-        //                         }
-        //                     ]
-        //                 }
-        //             ]
-        //         }
-        //     ],
-        // }
-        
-    }
-    
-    renderDropdown(detectedLanguages){
-        let languageOptions = detectedLanguages.map(
-            (lang, i) => <option>{lang}</option>
-        )
-        return (
-            <select onChange={(e) => this.setState(detectedLanguages: e.target.value)}>
-                {languageOptions}
-            </select>
-        )
+        const req = new Request(`http://localhost:8080?repo=${props.repo}`)
+        this.state = { results: {} }
+        fetch(req).then(val => val.json())
+            .then(json => {
+                this.setState({ results: json })
+            })
     }
 
-    renderPrintButton(){
-        return (
-            <button onClick={this.exportAsPdf}>Export as pdf</button>
-        );
+    render() {
+        return <>
+            <a href={`https://github.com/${this.props.repo}`}><RepoName>{this.props.repo}</RepoName></a>
+            {Object.keys(this.state.results).map(lang => {
+                return <div key={lang}>
+                    <h3>{lang} files</h3>
+                    {this.renderLanguage(this.state.results[lang])}
+                </div>
+            })}
+            <h3>.js files</h3>
+            <h3>.rb files</h3>
+        </>
     }
 
-    exportAsPdf(){
+    renderLanguage(files) {
+        return files.map(file => {
+            return <FileContainer>
+                <FileName>{file.Filename}</FileName>
+                {this.renderFunctions(file.ExportedFuncs)}
+            </FileContainer>
+        })
+    }
+
+    renderFunctions(fns) {
+        return <>
+            <GridContainer>
+                {fns.map(fn => <FunctionName>{fn.Name}()</FunctionName>)}
+            </GridContainer>
+        </>
+    }
+
+    exportAsPdf() {
         window.print();
     }
-}
-
-
-
-function File(props){
-    let functions = props.functions.map((func, i) => (Function(func)));
-    let types = props.types.map((type, i) => (Type(type)));
-    return (
-        <FileWrapper>
-            <Filename>{props.name} </Filename>
-            <FunctionsWrapper>
-                {functions}
-            </FunctionsWrapper>
-            <TypesWrapper>
-                {types}
-            </TypesWrapper>
-        </FileWrapper>
-    );
-}
-
-function Function(props){
-    let name = props.name;
-    let parameters = props.parameters.join(", ");
-    let returnValue = props.returns;
-    return <>
-        <FunctionKeyword>{"function "}</FunctionKeyword><FunctionName>{name}</FunctionName>
-        <br></br>
-        <Heading>{"Parameters: "}</Heading><Parameters>{parameters}</Parameters>
-        <br></br>
-        <Heading>{"Returns: "}</Heading><ReturnValue>{returnValue}</ReturnValue>
-        <br></br>
-        <br></br>
-    </>
-}
-
-function Type(props){
-    let name = props.name;
-    let fields = props.fields.map((f, i) => f.type + ' ' + f.name});
-    console.log(fields);
-    console.log(fields);
-    fields = fields.join(", ");
-    console.log(fields);
-    return <>
-        <StructKeyword>{"struct "}</StructKeyword><StructName>{name}</StructName>
-        <br></br>
-        <Heading>{"Fields: "}</Heading><Parameters>{fields}</Parameters>
-        <br></br>
-    </>
 }
