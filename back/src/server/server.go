8a5d549a539609aca6bfe9b5bed785917795355d
Maciej Goszczycki <mgoszcz2@gmail.com>
2018-09-16 01:13:47 -0400

    Kinda working server.go

--- back/src/server/server.go
+++ back/src/server/server.go
@@ -1,32 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github"
+	"goparse"
 	"net/http"
 	"os"
 )
 
+var ghc github.Client
+
 func main() {
-	http.HandleFunc("/parse", Respond)
+	ghc = github.NewClient()
+	http.HandleFunc("/", respond)
+	fmt.Println("Serving on :8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error serving: %v", err)
 	}
 }
 
-func Respond(w http.ResponseWriter, req *http.Request) {
-	path, ok := req.URL.Query()["path"]
-	if !ok {
-		http.Error(w, "no path provided", http.StatusBadRequest)
+func getFiles(repo string) ([]byte, error) {
+	links, err := github.GetLinks(repo, ghc, []string{".go"})
+	if err != nil {
+		return nil, err
+	}
+	files := make([]*goparse.File, len(links))
+	for i, link := range links {
+		resp, err := http.Get(link)
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode != 200 {
+			return nil, fmt.Errorf("Bad status")
+		}
+		file, err := goparse.ParseFile(link, resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		files[i] = file
+	}
+
+	return json.Marshal(files)
+}
+
+func respond(w http.ResponseWriter, req *http.Request) {
+	repo := req.URL.Query().Get("repo")
+	val, err := getFiles(repo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	resp, err := http.Get(path[0])
-    if err != nil {
-		http.Error(w, "could not get path", http.StatusInternalServerError)
-		return
-	}
-	if resp.StatusCode != http.StatusOK {
-		http.Error(w, "could not get path", http.StatusInternalServerError)
-		return
-	}
+	w.Write(val)
+	getFiles(repo)
 }
