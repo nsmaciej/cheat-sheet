e1de62f2301a1c4a93623e2a3fe6e4840a2c237b
dmac <DaraMac@users.noreply.github.com>
2018-09-16 07:02:12 +0100

    Added initial python files

---
+++ back/src/pyparse/testFunctions.py
@@ -0,0 +1,15 @@
+def nameHello(s, t):
+	return 'Hello' + s
+
+def annotatedFunc(i: int) -> int:
+	return i + 1
+
+def constantFunc():
+	return 100
+
+class TestClass:
+	def __init__(self):
+		self.val = 0
+
+	def testMethod(self, j):
+		return j
